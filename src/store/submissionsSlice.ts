08bdcd85708433b5a67068bb63dd859298161e74
Serg <2778665@gmail.com>
2026-06-08 22:56:02 +0300

    add: add redux store

---
+++ src/store/submissionsSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Submission } from '../types';
+
+type SubmissionInput = Omit<Submission, 'id' | 'isNew'>;
+
+const submissionsSlice = createSlice({
+  name: 'submissions',
+  initialState: [] as Submission[],
+  reducers: {
+    addSubmission(state, action: PayloadAction<SubmissionInput>) {
+      state.push({ ...action.payload, id: crypto.randomUUID(), isNew: true });
+    },
+    markAsRead(state, action: PayloadAction<string>) {
+      const item = state.find((s) => s.id === action.payload);
+      if (item) item.isNew = false;
+    },
+  },
+});
+
+export const { addSubmission, markAsRead } = submissionsSlice.actions;
+export default submissionsSlice.reducer;
